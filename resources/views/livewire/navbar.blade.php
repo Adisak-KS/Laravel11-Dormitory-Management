1b252766ddda96199080e999a5245f0ec67c9def
Adisak Khongsuk <Adisak.General@gmail.com>
2024-11-29 18:25:01 +0700

    Component Modal

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -5,10 +5,28 @@
             <span class="username">{{ $user_name }}</span>
         </div>
         <div>
-            <button wire:click="logout" class="border border-orange-400 text-orange-400 px-6 py-3 rounded-2xl">
+            <button wire:click="showModal = true" class="border border-orange-400 text-orange-400 px-6 py-3 rounded-2xl">
                 <span>ออกจากระบบ</span>
-                <i class="fa-solid fa-sign-out ms-3"></i>
+                <i class="fa-solid fa-sign-out-alt ms-2"></i>
             </button>
         </div>
     </div>
+
+    <x-modal wire:model="showModal" maxWidth="sm" title="ออกจากระบบ">
+        <div class="text-center">
+            <div><i class="fa-solid fa-question text-red-500 text-5xl"></i></div>
+            <div class="text-3xl font-bold text-gray-800 mt-3">ออกจากระบบ</div>
+            <div class="text-gray-800 mt-3 text-2xl">คุณต้องการออกจากระบบหรือไม่</div>
+        </div>
+        <div class="flex justify-center mt-6 pb-4">
+            <button class="btn-danger mr-2" wire:click="logout">
+                <i class="fa-solid fa-check"></i>
+                ยืนยัน
+            </button>
+            <button class="btn-secondary" wire:click="showModal = false">
+                <i class="fa-solid fa-xmark"></i>
+                ยกเลิก
+            </button>
+        </div>
+    </x-modal>
 </div>
