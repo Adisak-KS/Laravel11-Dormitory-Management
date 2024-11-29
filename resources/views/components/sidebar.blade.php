c7f75053738140595c0f154f7e3efe12cd4be5f7
Adisak Khongsuk <Adisak.General@gmail.com>
2024-11-29 16:36:23 +0700

    Dashboard System

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,30 @@
+<div class="navbar">
+    <div class="flex items-center justify-between">
+        <div>
+            <i class="fa-solid fa-user me-2"></i>
+            <span class="username">Admin System (Admin)</span>
+        </div>
+        <div>
+            <button class="border border-orange-400 text-orange-400 px-6 py-3 rounded-2xl">
+                <span>ออกจากระบบ</span>
+                <i class="fa-solid fa-sign-out ms-3"></i>
+            </button>
+        </div>
+    </div>
+</div>
+
+<div class="sidebar">
+    <div class="sidebar-header">
+        <div class="text-center">jApartment 1.0</div>
+    </div>
+    <div class="sidebar-body">
+        {{-- menu  --}}
+        <div class="menu">
+            <ul>
+                <li> <i class="fa-solid fa-house me-2"></i>Dashboard </li>
+                <li><i class="fa-solid fa-user me-2"></i>Profile</li>
+                <li> <i class="fa-solid fa-gear me-2"></i> Setting</li>
+            </ul>
+        </div>
+    </div>
+</div>
