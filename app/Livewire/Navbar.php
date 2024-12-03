7165f8b39f6c452e0125a27c374b1800aab4fbca
Adisak Khongsuk <Adisak.General@gmail.com>
2024-12-03 20:52:25 +0700

    Update Profile

--- app/Livewire/Navbar.php
+++ app/Livewire/Navbar.php
@@ -2,18 +2,61 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Navbar extends Component
 {
     public $user_name;
     public $showModal = false;
+    public $showModalEdit = false;
+    public $username;
+    public $password;
+    public $password_confirm;
+    public $errorUsername;
+    public $errorPassword;
+    public $saveSuccess = false;
 
-    public function mount(){
+    public function editProfile()
+    {
+        $this->showModalEdit = true;
+
+        $user = User::find(session()->get('user_id'));
+        $this->username = $user->name;
+        $this->saveSuccess = false;
+    }
+
+    public function updateProfile()
+    {
+        if ($this->username == '') {
+            $this->addError('username', 'กรุณากรอก Username');
+            return;
+        }
+
+        if ($this->password != $this->password_confirm) {
+            $this->addError('password_confirm', 'รหัสผ่านไม่ตรงกัน');
+            return;
+        }
+
+        $user = User::find(session()->get('user_id'));
+        $user->name = $this->username;
+        $user->password = $this->password ?? $user->password;
+        $user->save();
+
+        // $this->showModalEdit = false;
+        $this->saveSuccess = true;
+
+        // clear error
+        // $this->clearErrors();
+    }
+
+    public function mount()
+    {
         $this->user_name = session()->get('user_name');
     }
 
-    public function logout(){
+    public function logout()
+    {
         session()->flush();
         return redirect('/');
     }
--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -5,6 +5,12 @@
             <span class="username">{{ $user_name }}</span>
         </div>
         <div>
+            <button wire:click="editProfile"
+                class="border border-orange-300 text-blue-300  px-6 py-3 rounded-2xl hover:bg-blue-400 hover:text-white mr-2">
+                <i class="fa-solid fa-user mr-2"></i>
+                แก้ไขข้อมูลส่วนตัว
+
+            </button>
             <button wire:click="showModal = true" class="border border-orange-400 text-orange-400 px-6 py-3 rounded-2xl">
                 <span>ออกจากระบบ</span>
                 <i class="fa-solid fa-sign-out-alt ms-2"></i>
@@ -29,4 +35,43 @@
             </button>
         </div>
     </x-modal>
+
+
+    <x-modal wire:model="showModalEdit" maxWidth="sm" title="แก้ไขข้อมูลส่วนตัว">
+        @if ($errors->any())
+            <div class="alert-danger">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+
+        <div>Username</div>
+        <input type="text" class="form-control" wire:model="username">
+
+        <div class="mt-3">Password ใหม่</div>
+        <input type="password" class="form-control" wire:model="password">
+
+        <div class="mt-5">ยืนยัน Password ใหม่</div>
+        <input type="password" class="form-control" wire:model="password_confirm">
+
+        <div class="flex justify-center mt-6 pb-4">
+            <button class="btn-primary mr-2" wire:click="updateProfile">
+                <i class="fa-solid fa-check mr-2"></i>
+                ยืนยัน
+            </button>
+            <button class="btn-secondary" wire:click="showModalEdit = false">
+                <i class="fa-solid fa-xmark mr-2"></i>
+                ยกเลิก
+            </button>
+        </div>
+
+        @if ($saveSuccess)
+            <div class="alert-success alert">
+                <i class="fa-solid fa-check mr-2"></i>
+                บันทึกข้อมูลสำเร็จ
+            </div>
+        @endif
+    </x-modal>
+
 </div>
